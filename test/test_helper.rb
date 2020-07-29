cd0b0ab2757829eba6579bdca6a1f673722f3aa7
Ubuntu <ubuntu@ip-172-31-35-248.ap-northeast-1.compute.internal>
2020-07-29 11:39:47 +0000

    基本的なログイン機構（認証システム）の実装

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,10 +6,13 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
   # Add more helper methods to be used by all tests here...
 end
