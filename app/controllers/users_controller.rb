fa210d6f517ae984eeff78e512b7d3a868463abc
Ubuntu <ubuntu@ip-172-31-35-248.ap-northeast-1.compute.internal>
2020-07-30 12:33:03 +0000

    永続的セッション機能のの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -19,6 +20,20 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
     def user_params
@@ -26,4 +41,10 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
   
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
 end
