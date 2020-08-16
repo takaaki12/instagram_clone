adbebdbf872200ec22972544c9d2f70045dcb781
Ubuntu <ubuntu@ip-172-31-35-248.ap-northeast-1.compute.internal>
2020-08-16 05:10:39 +0000

    投稿機能実装（本番環境での画像投稿はまだできていない）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+
   end
   
   def new
@@ -54,22 +56,14 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
   
-  def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-  end
-  
-  def correct_user
-    @user = User.find(params[:id]) 
-    redirect_to(root_url) unless current_user?(@user)
-  end
-  
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    def correct_user
+      @user = User.find(params[:id]) 
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 
 end
