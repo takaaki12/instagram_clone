fa71fa014525d733d14998c61909d4af856abe20
Ubuntu <ubuntu@ip-172-31-35-248.ap-northeast-1.compute.internal>
2020-09-12 08:31:07 +0000

    micropostsの削除エラー改善

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :likes
-  has_many :liked_users, through: :likes, source: :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user, dependent: :destroy
   has_many :notifications, dependent: :destroy
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
